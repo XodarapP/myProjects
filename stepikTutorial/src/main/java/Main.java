a0e2f16ea9b9ecf4ccbce79c911748d033001e6d
xodarapP <bpn20@yandex.ru>
2017-12-11 15:28:46 +0700

    server

---
+++ stepikTutorial/src/main/java/Main.java
@@ -0,0 +1,20 @@
+import servlets.Frontend;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Frontend frontend = new Frontend();
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(frontend), "/mirror");
+
+        Server server = new Server(8080);
+        server.setHandler(context);
+
+        server.start();
+        System.out.println("Server started");
+        server.join();
+    }
+}
